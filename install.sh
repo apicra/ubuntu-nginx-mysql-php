6a0db614c89bc8307f2a80fc6893dd28cc1e755b
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2021-08-24 11:36:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 sudo apt update
 # NGINX
-sudo apt install nginx -y
+sudo apt install -y nginx
 # PHP
-sudo apt install php-fpm -y
+sudo apt install -y php-fpm
+sudo apt install -y php7.4-cli php7.4-fpm php7.4-curl php7.4-gd php7.4-mysql php7.4-mbstring zip unzip
 # FIREWALL
 sudo ufw allow 'Nginx Full'
 # MYSQL
-sudo apt install mysql-server -y
+sudo apt install -y mysql-server
 sudo mysql_secure_installation
